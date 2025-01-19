bb8fbbe87f0985bf4bdd595823da79cc37dcf8b8
Andrii Slabkyi <145043179+Polash0t@users.noreply.github.com>
2025-01-19 20:26:45 +0200

    stoped for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import StatusLabel from '@/app/components/status-label';
+import StatusLabel, {Status} from '@/app/components/status-label';
+import { cookies } from 'next/headers';
 
 export default function Home() {
+  console.log(cookies())
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
-      <StatusLabel>Active</StatusLabel>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended} disabled={true}>Suspended</StatusLabel>
     </main>
   );
 }
